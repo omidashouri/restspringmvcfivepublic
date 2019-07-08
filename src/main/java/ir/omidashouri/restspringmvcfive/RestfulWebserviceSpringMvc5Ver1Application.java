cec5a3248488be5f64a929f6cef346619738f85f
o.ashouri <omidashouri@gmail.com>
2019-07-08 10:43:49 +0430

    Initial commit : omidashouri : 97.04.17

---
+++ src/main/java/ir/omidashouri/restspringmvcfive/RestfulWebserviceSpringMvc5Ver1Application.java
@@ -0,0 +1,13 @@
+package ir.omidashouri.restspringmvcfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebserviceSpringMvc5Ver1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebserviceSpringMvc5Ver1Application.class, args);
+    }
+
+}
---
+++ src/test/java/ir/omidashouri/restspringmvcfive/RestfulWebserviceSpringMvc5Ver1ApplicationTests.java
@@ -0,0 +1,16 @@
+package ir.omidashouri.restspringmvcfive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWebserviceSpringMvc5Ver1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
