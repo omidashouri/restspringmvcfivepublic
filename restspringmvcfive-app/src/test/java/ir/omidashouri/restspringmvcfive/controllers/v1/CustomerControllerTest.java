8f2571f32e113c3dca8c1cbe981bf09a6b87b487
o.ashouri <omidashouri@gmail.com>
2019-09-25 12:07:29 +0330

    add comment to specify integration test : omidashouri : 98.07.03

--- restspringmvcfive-app/src/test/java/ir/omidashouri/restspringmvcfive/controllers/v1/CustomerControllerTest.java
+++ restspringmvcfive-app/src/test/java/ir/omidashouri/restspringmvcfive/controllers/v1/CustomerControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import java.util.Arrays;
 
 
@@ -186,6 +187,7 @@ public class CustomerControllerTest {
         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
+//        verify if this method is running
         Mockito.verify(customerService).deleteCustomerById(ArgumentMatchers.anyLong());
     }
 
